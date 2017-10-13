a4219c7319d182e4b8e448b92def3dd938ae4f4f
wangxiaoyun <wangxiaoyun@zhufaner.com>
2017-10-13 11:51:54 +0800

    test

---
+++ app/src/main/java/com/android/librarywork_test/MainActivity.java
@@ -0,0 +1,28 @@
+package com.android.librarywork_test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+@Route(path = "/workModule/2")
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R2.id.tv_test)
+    TextView tvTest;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_activity_main);
+        ButterKnife.bind(this);
+        tvTest.setText(getIntent().getStringExtra("test"));
+        Intent intent = new Intent();
+        intent.putExtra("test", "业务模块二返回值");
+        setResult(RESULT_OK, intent);
+    }
+}
